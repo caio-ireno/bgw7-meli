6481e9c79c6f560df719593ca3148f468343b9a3
caio-ireno <caio_ireno@hotmail.com>
2025-05-29 19:16:23 -0300

    variaveis estudo

---
+++ Go-Study/variaveis/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Oi caio, vamos criar nosso app?")
+
+	var nome string = "caio"
+	// var n1 int = 10
+	var n1 int = -10
+	var n2 uint = 11
+	// var n2 uint = -11 // ERRO NUMERC OVERFLOW
+
+	fmt.Println(nome)
+	fmt.Println(n1)
+	fmt.Println(n2)
+
+}
